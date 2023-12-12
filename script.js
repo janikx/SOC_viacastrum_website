9fc6521cf9cb4eff3b6d2afe4b174541080909dd
jankwa <79253266+jankauvu@users.noreply.github.com>
2023-12-12 22:59:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var numberone = Number(prompt("Zadaj cislo:"));
-var numbertwo = Number(prompt("Zadaj druhe cislo:"));
-var sign = prompt("Zadaj znak:");
 
-if (sign == "+") {
-    console.log("Vysledok scitania: " + numberone + numbertwo);
-} else if (sign == "-") {
-    console.log("Vysledok odcitania: " + numberone - numbertwo);
-} else if (sign == "*") {
-    console.log("Vysledok nasobenia: " + numberone * numbertwo);
-} else if (sign == "/") {
-    console.log("Vysledok delenia: " + numberone / numbertwo);
-} else {
-    console.log("Nie je mozne vyriesit tento problem.");
-}
